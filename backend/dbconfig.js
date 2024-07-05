c0290fe79fd2c0149e5ac41496a898c1cd2c9e3a
JaffDavy <jaffdavy100@gmail.com>
2024-07-05 15:14:55 +0100

    added table in dbconfig.js

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -5,15 +5,43 @@ const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOSTNAME,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
-pool
-  .connect()
-  .then(() => console.log("Connected to the database on port 5432"))
+pool.connect()
+  .then(() => {
+    console.log("Connected to the database on port 5432")
+    const checkingTheTableQUery = `
+      SELECT EXIST (
+      SELECT FROM information_shema.tables
+      WHERE table_schema = 'public'
+      AND table_name = 'entries'
+      )`;
+
+    const createTableQuery = `
+      CREATE TABLE entries (
+      id SERIAL INT PRIMARY KEY,
+      date DATE,
+      description VARCHAR(200),
+      weather TEXT,
+      temparature FLOAT
+      )`
+
+    pool.query((checkingTheTableQUery)
+      .then(result => {
+        if (!result.rows[0].exist) {
+          return pool.query(createTableQuery);
+        }
+      })
+      .catch(err => {
+        console.error('Error executing query', err);
+      })
+    )
+  })
+
   .catch((err) => console.error(err));
 
 export default pool;
